57f5aa1f2f3d8dc9ce15a3a9fea22df3cad3bbe8
Siva Singanapalli <sivasinganapalli@gmail.com>
2024-06-07 23:59:32 +0530

    Lower and Upper case

---
+++ java_100dayCoding/day5/LowerAndUpper.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class LowerAndUpper {
+
+    public static void main(String[] args) {
+        String s = "ajfhJGGkjgkjgLKGLgkghjfjfJVLjljhfHVhvkFKFKHVHVKHJFKJKkfkfkjvk";
+        System.out.println("Converting the given string to UpperCase : " + s.toUpperCase());
+        System.out.println("Converting the given string to UpperCase : " + s.toLowerCase());
+    }
+
+}
